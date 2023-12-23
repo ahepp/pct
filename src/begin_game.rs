f3749ddec4d1fd24543d08ba449aa5f9551d7063
Andrew Hepp <andrew.hepp@ahepp.dev>
2023-12-23 18:00:39 -0500

    add beginning of game

---
+++ src/begin_game.rs
@@ -0,0 +1,16 @@
+use crate::state::State;
+
+pub struct BeginGame;
+impl BeginGame {
+    pub fn new() -> Self {
+        BeginGame
+    }
+}
+impl State for BeginGame {
+    fn render(&self) {
+        println!("Welcome to the Pacific Crest Trail!");
+    }
+    fn next_state(self: Box<Self>) -> Option<Box<dyn State>> {
+        None
+    }
+}
--- src/run.rs
+++ src/run.rs
@@ -1,3 +1,14 @@
+use crate::begin_game::BeginGame;
+use crate::state::State;
+
 pub fn run() {
-    println!("Hello, world!");
+    let mut state: Box<dyn State> = Box::new(BeginGame::new());
+    loop {
+        state.render();
+        state = match state.next_state() {
+            None => return,
+            Some(state) => state,
+        };
+        std::thread::sleep(std::time::Duration::from_millis(1000));
+    }
 }
