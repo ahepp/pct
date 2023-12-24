7043aafd58a6b2bed781ff8581b502ec808847b5
Andrew Hepp <andrew.hepp@ahepp.dev>
2023-12-24 02:44:32 -0500

    add end of game

--- src/begin_game.rs
+++ src/begin_game.rs
@@ -1,3 +1,4 @@
+use crate::end_game::EndGame;
 use crate::state::State;
 
 pub struct BeginGame;
@@ -11,6 +12,6 @@ impl State for BeginGame {
         println!("Welcome to the Pacific Crest Trail!");
     }
     fn next_state(self: Box<Self>) -> Option<Box<dyn State>> {
-        None
+        Some(Box::new(EndGame))
     }
 }
