ee6111ec84a07c86154ee42e5fcd9c1cfe5fdf66
Andrew Hepp <andrew.hepp@ahepp.dev>
2023-12-31 01:30:15 -0500

    render start of day

--- src/scenes/start_day.rs
+++ src/scenes/start_day.rs
@@ -3,6 +3,9 @@ use crate::scene::Scene;
 use crate::states::StartDay;
 
 impl Scene for StartDay {
-    fn render(&self, _ctx: &Context) {
+    fn render(&self, ctx: &Context) {
+        let day_number = ctx.day + 1;
+        println!("You started day {} at {:.2}", day_number, ctx.mm);
+        util::render_delay();
     }
 }
