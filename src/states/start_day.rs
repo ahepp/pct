13750783501f1f52a1f1727ff3eac45e64eeee80
Andrew Hepp <andrew.hepp@ahepp.dev>
2023-12-31 01:25:11 -0500

    start day transitions to start day

--- src/states/start_day.rs
+++ src/states/start_day.rs
@@ -4,11 +4,27 @@ use crate::state::State;
 pub struct StartDay;
 impl State for StartDay {
     fn next_state(&self, ctx: Context) -> (Option<Box<dyn State>>, Context) {
-        (None, ctx)
+        (Some(Box::new(StartDay)), ctx)
     }
 
     #[cfg(test)]
     fn as_any(&self) -> &dyn std::any::Any {
         self
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn next_state_start_day() {
+        let ctx = Context;
+        let state = StartDay;
+        let state = match state.next_state(ctx) {
+            (Some(state), _) => state,
+            _ => panic!(),
+        };
+        assert!(state.as_any().is::<StartDay>());
     }
 }
